ec88d6a9b17f5cbf375abdb4a635ddc126a204f2
TJ <tjcarney@stny.rr.com>
2016-12-22 11:57:38 -0500

    Completed Playground Assignment

--- Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@
 /*: question1
  ### 1. Create a tuple to represent a person! The tuple should have a person's name in the first field and their age in the second field. Define the tuple as a variable named `person`.
  */
-// write your code here
+var person = (name: "TJ", age: 27)
 
 
 
@@ -26,7 +26,7 @@
 /*: question2
  ### 2. Print out "<Name> is <Age> years old" using the tuple you created in Question 1.
  */
-// write your code here
+print("\(person.name) is \(person.age) years old")
 
 
 
@@ -38,7 +38,8 @@
 /*: question3
  ### 3. It is the person's birthday! Increment the person's age by one year that you created in Question 1.
  */
-// write your code here
+person.age = 28
+
 
 
 
@@ -50,7 +51,7 @@
 /*: question4
  ### 4. Print out "Happy <Age> birthday, <Name>!" using the `person` variable.
  */
-// write your code here
+print("Happy \(person.age)th birthday, \(person.name)!)")
 
 
 
@@ -62,7 +63,7 @@
 /*: question5
  ### 5. Create another person variable called `someone` using _named_ fields for the name and age.
  */
-// write your code here
+var someone = (called: "Sean", number: 28)
 
 
 
@@ -74,7 +75,7 @@
 /*: question6
  ### 6. Print out "<Name> is <Age> years old" for the `someone` variable you created in Question 5.
  */
-// write your code here
+print("\(someone.called) is \(someone.number) years old")
 
 
 
@@ -85,7 +86,7 @@
 /*:
  ### 7. It is `someone`'s birthday! Increment `someone`'s age by one year.
  */
-// write your code here
+someone.number = 29
 
 
 
@@ -96,7 +97,7 @@
 /*:
  ### 8. Print out "Happy <Age> birthday, <Name>!" using the `someone` variable.
  */
-// write your code here
+print("Happy \(someone.number)th birthday, \(someone.called)!")
 
 
 
@@ -108,7 +109,7 @@
 /*:
  ### 9. Create a _constant_ tuple called `human`, with named fields for `name` and `age`.
  */
-// write your code here
+let human = (name: "Tara", age: 29)
 
 
 
@@ -120,7 +121,7 @@
 /*:
  ### 10. Can you change `human`'s age? Why or why not?
  */
-
+//No, it is a let constant. 
 
 
 
